c3b97650f114679feb30ff3927c663b7dd0658b4
001AJD <ajinkyadhomne.27@gmail.com>
2020-04-29 17:59:32 +0530

    first commit

---
+++ Customer.svc.cs
@@ -0,0 +1,49 @@
+﻿using OnlineStoreRestApi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace OnlineStoreRestApi
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Customer" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Customer.svc or Customer.svc.cs at the Solution Explorer and start debugging.
+    public class Customer : ICustomer
+    {
+     
+
+        public List<CustomerList> GetAllCustomer()
+        {
+            List<CustomerList> custList;
+            using (var ctx = new db_onlineStoreEntities())
+            {
+                custList = ctx.sp_getAllUser1().ToList<CustomerList>();
+                
+            }
+            return custList;
+        }
+
+        public string RegisterCustomer(string email, string password)
+        {
+            string response = "success";
+
+            if (email == null && password == null)
+            {
+                response = "invalid data";
+            }
+            else
+            {
+                using (var ctx = new db_onlineStoreEntities())
+                {
+                    int num;
+                    num = ctx.registerCustomer(email, password);
+
+                    response = num.ToString();
+                }
+            }
+            return response;
+        }
+    }
+}
---
+++ ICustomer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using OnlineStoreRestApi.Model;
+
+namespace OnlineStoreRestApi
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ICustomer" in both code and config file together.
+    [ServiceContract]
+    public interface ICustomer
+    {
+        [OperationContract]
+        [WebGet(RequestFormat = WebMessageFormat.Xml,
+            ResponseFormat = WebMessageFormat.Xml,
+            UriTemplate = "/GetAllCustomer")]
+        List<CustomerList> GetAllCustomer();
+
+
+        [OperationContract]
+        [WebInvoke( Method = "POST",
+            RequestFormat = WebMessageFormat.Xml,
+            ResponseFormat = WebMessageFormat.Xml,
+            UriTemplate = "/RegisterCustomer",
+            BodyStyle = WebMessageBodyStyle.Wrapped
+            )]
+        string RegisterCustomer(string email, string mobile);
+    }
+}
